3435d81d99e0d7941f8492fd4d537b0776431590
Diegoramos115 <alejoramos8302@gmail.com>
2025-08-27 19:47:59 -0500

    Segundo commit

--- app/src/main/java/com/example/taler2/HomeActivity.kt
+++ app/src/main/java/com/example/taler2/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.example.taler2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -15,6 +18,11 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+        val tvRegister: TextView = findViewById(R.id.tv_reg)
 
+        tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
